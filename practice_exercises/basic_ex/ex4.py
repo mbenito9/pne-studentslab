59653cdbe0f385a7b89ced3cb99bbd7912dad738
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-02-15 18:03:34 +0100

    ask why my flag does not work

--- practice_exercises/basic_ex/ex4.py
+++ practice_exercises/basic_ex/ex4.py
@@ -1,18 +1,19 @@
-flag = True
-while flag:
-    score = input("Enter a numeric score")
-    if score == "stop":
-        flag = False
-        print("You stopped analysizing grades")
-    else:
-        score = int(score)
-        if 0.0 <= score <= 2.9:
-            print("F")
-        elif 3.0 <= score <= 4.9:
-            print("D")
-        elif 5.0 <= score <= 6.9:
-            print("C")
-        elif 7.0 <= score <= 8.9:
-            print("B")
-        elif 9.0 <= score <= 10.0:
-            print("A")
+if __name__ == "__main__":
+    flag = True
+    while flag:
+        score = input("Enter a numeric score")
+        if score == "no":
+            flag = False
+            print("You stopped analysizing grades")
+        else:
+            score = float(score)
+            if 0.0 <= score <= 2.9:
+                print("F")
+            elif 3.0 <= score <= 4.9:
+                print("D")
+            elif 5.0 <= score <= 6.9:
+                print("C")
+            elif 7.0 <= score <= 8.9:
+                print("B")
+            elif 9.0 <= score <= 10.0:
+                print("A")
