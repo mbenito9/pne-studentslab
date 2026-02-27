a52777d9c5be4fddc7ecdf3ae8f9e140a23a15b4
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-02-27 14:26:23 +0100

    EX4

--- P01/e4.py
+++ P01/e4.py
@@ -4,11 +4,10 @@ s1 = Seq()
 s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
 
-l = []
-l.append(s1)
-l.append(s2)
-l.append(s3)
+l = [s1, s2, s3]
 
+print("-----| Practice 1, Exercise 4 |------")
 for i in l:
-    length = len(i)
-    print(f"Sequence {index + 1}: {i}")
+    index = l.index(i)
+    length = i.len()
+    print(f"Sequence {index + 1}: (Length: {length}) {i}")
