20fc42fb35f2f90b0c34bf197004c7cf2d9a4f49
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-03-02 11:29:28 +0100

    seq01

--- P01/Seq1.py
+++ P01/Seq1.py
@@ -3,7 +3,7 @@ from pathlib import Path
 class Seq:
     def __init__(self, strbases=None):
         self.bases = strbases
-        if strbases != None:
+        if strbases is not None:
             b = ["A", "G", "C", "T"]
             non_valid = 0
             for i in self.bases:
@@ -37,3 +37,40 @@ class Seq:
         else:
             c = 0
         return c
+    def count(self):
+        dct = {"A": 0, "T": 0, "C": 0, "G": 0}
+        if self.bases != "NULL" and self.bases != "ERROR":
+            for i in self.bases:
+                dct[i] += 1
+        return dct
+    def reverse(self):
+        if self.bases == "NULL":
+            f = "NULL"
+        elif self.bases == "ERROR":
+            f = "ERROR"
+        else:
+            f = self.bases[::-1]
+        return f
+    def complement(self):
+        if self.bases == "NULL":
+            new_seq = "NULL"
+        elif self.bases == "ERROR":
+            new_seq = "ERROR"
+        else:
+            bases = {"A": "T", "T": "A", "C": "G", "G": "C"}
+            new_seq = ""
+            for i in self.bases:
+                complement = bases[i]
+                new_seq += complement
+        return new_seq
+    def read_fasta(self,filename):
+        if self.bases == "NULL":
+            folder = "sequences/"
+            complete = folder + filename
+            read = Path(complete).read_text()
+            index = read.find("\n")
+            string = read[index::]
+            final = string.replace("\n", "")
+            self.bases = final
+
+
