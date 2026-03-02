91a67f83d9056c13c83488075347dbe588ed6a4b
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-03-02 10:45:45 +0100

    e8

---
+++ P01/e8.py
@@ -0,0 +1,16 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+l = [s1, s2, s3]
+
+for sequence in l:
+    i = l.index(sequence)
+    length = sequence.len()
+    print(f"Sequence {i}: (Length: {length}) {sequence}")
+    dct = sequence.count()
+    print(" Bases:", dct)
+    print(" Reverse:", sequence.reverse())
+    print(" Complement:", sequence.complement())
