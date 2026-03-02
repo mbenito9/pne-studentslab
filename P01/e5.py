5312be25465fb5f2ed38ac42256f03a1e11ecd3c
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-03-02 09:59:12 +0100

    EX5

--- P01/e5.py
+++ P01/e5.py
@@ -6,4 +6,16 @@ s3 = Seq("Invalid sequence")
 
 l = [s1, s2, s3]
 
-dct = {"A": 0 , "T": 0, "C": 0, "G": 0}
+bases = ["A", "T", "C", "G"]
+
+for sequence in l:
+    i = l.index(sequence)
+    length = sequence.len()
+    print(f"Sequence {i}: (Length: {length}) {sequence}")
+    c = 0
+    for base in bases:
+        c += 1
+        if c != len(bases):
+            print(f"{base}: {sequence.count_base(base)}", end=",  ")
+        else:
+            print(f"{base}: {sequence.count_base(base)}", end="\n")
