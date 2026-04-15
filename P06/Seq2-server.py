7759442f9b19707688e50095cc6e513a003f4478
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-04-15 19:00:59 +0200

    practice6

--- P06/Seq2-server.py
+++ P06/Seq2-server.py
@@ -2,6 +2,13 @@ import socketserver
 from pathlib import Path
 import http.server
 from urllib.parse import parse_qs, urlparse
+import jinja2 as j
+from Seq1 import Seq
+
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
 
 port = 8080
 
@@ -18,6 +25,40 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             c = Path("html/index.html").read_text()
         elif path == "/ping":
             c = Path("html/ping.html").read_text()
+        elif path == "/get":
+            genes = ["TTTGATCATAGTACTAG", "GTCATATAGAG", "CCCCTTTTG", "CCCCCCGGGA", "ATATCGCATCTCAGCTTC"]
+            num_req = arguments["number"]
+            num_req = int(num_req[0])
+            seq = genes[num_req]
+            dct = {"number": num_req, "seq": seq}
+            c = read_html_file("get.html").render(new=dct)
+        elif path == "/gene":
+            gene_name = arguments["gene_name"]
+            gene_name = gene_name[0]
+            seq = Seq()
+            gene = {"gene_name": gene_name, "gene": seq.read_fasta(gene_name + ".txt")}
+            print(seq.read_fasta(gene_name + ".txt"))
+            c = read_html_file("gene.html").render(gene=gene)
+        elif path == "/opert":
+            seq = arguments["msg"][0]
+            opert = arguments["operation"][0]
+            sequence = Seq(seq)
+            if opert == "Rev":
+                result = sequence.reverse()
+            elif opert == "Info":
+                total = sequence.len()
+                result = f"Total length: {total}\n"
+
+                dict_bases = sequence.count()
+                for base, count in dict_bases.items():
+                    pct = round((count / total) * 100, 1)
+                    ans = f"{base}: {count} ({pct}%)\n"
+                    result += ans
+            elif opert == "Comp":
+                result = sequence.complement()
+            dct = {"seq": seq, "opert": opert, "result": result}
+            c = read_html_file("operation.html").render(op=dct)
+
         else:
             c = Path("html/error.html").read_text()
         self.send_header("Content-Type", "text/html")
