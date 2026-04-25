f420626274fa41ad6de72781cf6e84b500388f03
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-04-25 18:52:28 +0200

    practice 7

--- P07/e2.py
+++ P07/e2.py
@@ -11,10 +11,6 @@ genes = {"FRAT1": "ENSG00000165879",
          "ANK2": "ENSG00000145362"
          }
 print("Dictionary of genes!")
-total = 0
-for key in genes.keys():
-    total += 1
-print(f"There are {total} genes in the dictionary")
+print(f"There are {len(genes)} genes in the dictionary:")
 for key, value in genes.items():
-    termcolor.cprint(key, "green", end=" " )
-    print(f"----> {value}")
+    print(f"{termcolor.colored(key, "green")} ----> {value}")
