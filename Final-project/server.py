328e602130f813808fffd7fcb887b1778264d11a
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-04-28 16:32:34 +0200

    till listspecies

--- Final-project/server.py
+++ Final-project/server.py
@@ -1,7 +1,15 @@
 import http.server
+import http.client
 import socketserver
 from pathlib import Path
 from urllib.parse import parse_qs, urlparse
+import json
+import jinja2 as j
+
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
 
 port = 8080
 socketserver.TCPServer.allow_reuse_address = True
@@ -11,7 +19,50 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         analyse_path = urlparse(self.path)
         path = analyse_path.path
         arg = parse_qs(analyse_path.query)
-
         if path == "/":
+            self.send_response(200)
             body = Path("html/main_page.html").read_text()
+        elif path == "/listSpecies":
+            self.send_response(200)
+            server = "rest.ensembl.org"
+            endp = f"/info/species"
+            params = "?content-type=application/json"
+            url = server + endp + params
 
+            conn = http.client.HTTPConnection(server)
+            conn.request("GET", endp + params)
+            ans = conn.getresponse()
+
+            dict_data = json.loads(ans.read().decode())
+            lst_species = dict_data["species"]
+            total = 0
+            if "limitval" in arg.keys():
+                limit = int(arg["limitval"][0])
+            else:
+                limit = 0
+            req_species = """
+            <ul>\n
+            """
+            for specie in lst_species:
+                name = specie["common_name"]
+                req_species += f"<li>{name}</li>\n"
+                total += 1
+                if total == limit:
+                    break
+            req_species += "</ul>"
+            dct = {"limit": limit, "total_species": len(lst_species), "names": req_species}
+            body = read_html_file("basic1.html").render(changes=dct)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(body.encode())))
+        self.end_headers()
+        self.wfile.write(body.encode())
+        return
+handler = TestHandler
+
+with socketserver.TCPServer(("", port), handler) as httpd:
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Server stopped by the user")
+        httpd.server_close()
