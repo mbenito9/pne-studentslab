17d0293bfb5901febf2494f22fce7e49ce9dd68c
mbenito9 <m.medranda.2025@alumnos.urjc.es>
2026-04-29 12:56:04 +0200

    revisar chromosome length

--- Final-project/server.py
+++ Final-project/server.py
@@ -19,61 +19,88 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         analyse_path = urlparse(self.path)
         path = analyse_path.path
         arg = parse_qs(analyse_path.query)
-        if path == "/":
+        try:
             self.send_response(200)
-            body = Path("html/main_page.html").read_text()
-        elif path == "/listSpecies":
-            self.send_response(200)
-            server = "rest.ensembl.org"
-            endp = "/info/species"
-            params = "?content-type=application/json"
-            url = server + endp + params
+            if path == "/":
+                body = Path("html/main_page.html").read_text()
 
-            conn = http.client.HTTPConnection(server)
-            conn.request("GET", endp + params)
-            ans = conn.getresponse()
+            elif path == "/listSpecies":
+                server = "rest.ensembl.org"
+                endp = "/info/species"
+                params = "?content-type=application/json"
+                url = server + endp + params
 
-            dict_data = json.loads(ans.read().decode())
-            lst_species = dict_data["species"]
-            total = 0
-            if "limitval" in arg.keys():
-                limit = int(arg["limitval"][0])
-            else:
-                limit = 0
-            req_species = """
-            <ul>\n
-            """
-            for specie in lst_species:
-                name = specie["common_name"]
-                req_species += f"<li>{name}</li>\n"
-                total += 1
-                if total == limit:
-                    break
-            req_species += "</ul>"
-            dct = {"limit": limit, "total_species": len(lst_species), "names": req_species}
-            body = read_html_file("basic1.html").render(changes=dct)
-        elif path == "/karyotype":
-            self.send_response(200)
-            server = "rest.ensembl.org"
-            specie = arg["specie"]
-            endp = f"/info/assembly/{specie}"
-            params = "?content-type=application/json"
-            url = server + endp + params
+                conn = http.client.HTTPConnection(server)
+                conn.request("GET", endp + params)
+                ans = conn.getresponse()
 
-            conn = http.client.HTTPConnection(server)
-            conn.request("GET", endp + params)
-            ans = conn.getresponse()
+                dict_data = json.loads(ans.read().decode())
+                lst_species = dict_data["species"]
+                total = 0
+                if "limitval" in arg.keys():
+                    limit = int(arg["limitval"][0])
+                else:
+                    limit = 0
+                req_species = """
+                <ul>\n
+                """
+                for specie in lst_species:
+                    name = specie["common_name"]
+                    req_species += f"<li>{name}</li>\n"
+                    total += 1
+                    if total == limit:
+                        break
+                req_species += "</ul>"
+                dct = {"limit": limit, "total_species": len(lst_species), "names": req_species}
+                body = read_html_file("basic1.html").render(changes=dct)
 
-            dict_data = json.loads(ans.read().decode())
-            karyo_lst = dict_data["karyotype"]
-            total_chr = """
-            <ul>\n
-            """
-            for karyo in karyo_lst:
-                total_chr += f"<li>{karyo}</li>\n"
-            total_chr += "</ul>"
-            dct = {"chroms": total_chr}
-            body = read_html_file("basic1.html").render(changes=dct)
+            elif path == "/karyotype" or path == "/chromosomeLength":
+                specie = arg["specie"][0]
+                server1 = "rest.ensembl.org"
+                endp1 = "/info/species"
+                params1 = "?content-type=application/json"
+
+                conn = http.client.HTTPConnection(server1)
+                conn.request("GET", endp1 + params1)
+                ans1 = conn.getresponse()
+
+                dict_data = json.loads(ans1.read().decode())
+                lst_species = dict_data["species"]
+                for i in lst_species:
+                    if i["common_name"] == specie:
+                        name_kry = i["name"]
+
+                server2 = "rest.ensembl.org"
+                endp = f"/info/assembly/{name_kry}"
+                params = "?content-type=application/json"
+
+                conn = http.client.HTTPConnection(server2)
+                conn.request("GET", endp + params)
+                ans = conn.getresponse()
+
+                dict_data = json.loads(ans.read().decode())
+                if path == "/karyotype":
+                    karyo_lst = dict_data["karyotype"]
+                    total_chr = """
+                    <ul>\n
+                    """
+                    for karyo in karyo_lst:
+                        total_chr += f"<li>{karyo}</li>\n"
+                    total_chr += "</ul>"
+
+                    dct = {"chroms": total_chr, "specie": specie}
+                    body = read_html_file("basic2.html").render(changes=dct)
+                elif path == "/chromosomeLength":
+                    n_chr = arg["chromo"][0]
+                    lst_region = dict_data["top_level_region"]
+                    for chromosome in lst_region:
+                        if lst_region.index(chromosome) == n_chr:
+                            length = chromosome["length"]
+                    dct = {"number": n_chr, "len": length}
+                    body = read_html_file("basic3.html").render(changes=dct)
+        except Exception:
+            self.send_response(404)
+            body = Path("html/error.html").read_text()
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", str(len(body.encode())))
         self.end_headers()
